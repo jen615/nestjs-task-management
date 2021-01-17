c044d6c1f7f56092c45fa13a138eeaf21ea16187
jen615 <xjenghisx@gmail.com>
2021-01-17 12:54:17 -0800

    additional authentication and jwt support

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator } from '@nestjs/common';
+import { User } from './user.entity';
+
+const GetUser = createParamDecorator((data, req): User => {
+    return req.user;
+  },
+);
